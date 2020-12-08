476c12cd42eb101ce6e3c60e99f41864523e4f37
Jasmin Kaset <kasetski@gmail.com>
2020-12-07 20:23:53 -0600

    logic works, app recognizes the difference between guests and users

--- src/components/home/HomeList.js
+++ src/components/home/HomeList.js
@@ -1,26 +1,40 @@
-import React, {useContext, useEffect} from "react"
+import React, { useContext, useEffect } from "react"
 import "./Home.css"
 import { ActivityContext } from "../activities/ActivityProvider"
 //import { Activity } from "../activities/Activity"
 
-export const HomeList = () => {
-  
-  // (find function through activities.userId, if any match current user, return existing user home / else return new user home)
-  const user = parseInt(localStorage.getItem("runnersHi_user"))
-  console.log(user)
+export const HomeList = (props) => {
 
-  const {activities, getActivities} = useContext(ActivityContext)
-  
+  // (find function through activities.userId, if any match current user, return existing user home / else return new user home)
+
+
+  const { activities, getActivities } = useContext(ActivityContext)
+
   useEffect(() => {
     console.log("ActivityList: Initial render before data")
     getActivities()
   }, [])
 
- 
 
 
-//if (activities.find(activity => activity.userId === user))
-  return (
-    <h1>you're home</h1>
-  )
+  const user = parseInt(localStorage.getItem('runnersHi_user'))
+  console.log(user)
+
+  const UserHome = 1111
+  const GuestHome = 3
+
+
+  let userHasActivities = activities.map(activity => activity.userId === user)
+  console.log(userHasActivities)
+
+
+  if (userHasActivities.includes(true, 0)) {
+    return <h3>Welcome back</h3>
+  } else {
+    return <h3>Track a new activity</h3>
+  }
+
+  return <p>sup </p>
+
+
 }
