7dd64dadd6dc435a6bc2508f6ac3d33610c9c9de
Jasmin Kaset <kasetski@gmail.com>
2020-12-08 06:51:31 -0600

    first link works

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,6 +1,28 @@
-// import React from "react"
-// import { Route } from "react-router-dom"
+import React from "react"
+import { Route } from "react-router-dom"
+import { ActivityList } from "./activities/ActivityList"
+import { ActivityProvider } from "./activities/ActivityProvider"
+import { ActivityTypeProvider } from "./activityTypes/ActivityTypeProvider"
+import { HomeList } from "./home/HomeList"
 
-// export const ApplicationViews = (props) => {
-  
-// }
+export const ApplicationViews = (props) => {
+  return (
+    <>
+
+      <ActivityProvider>
+        <Route exact path="/" render={
+          props => <HomeList {...props} />
+        } />
+      </ActivityProvider>
+
+      <ActivityTypeProvider>
+        <ActivityProvider>
+          <Route exact path="/activities">
+            <ActivityList />
+          </Route>
+        </ActivityProvider>
+      </ActivityTypeProvider>
+
+    </>
+  )
+}
