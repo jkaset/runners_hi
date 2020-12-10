f13f80c59c825ff4de3b54b1fe702a1fea7720a1
Jasmin Kaset <kasetski@gmail.com>
2020-12-10 17:11:52 -0600

    math works

--- src/components/activities/ActivityMoodMath.js
+++ src/components/activities/ActivityMoodMath.js
@@ -6,13 +6,17 @@ export default function ActivityMoodsMath() {
   const { activities } = useContext(ActivityContext)
   //Mood math 
   const user = parseInt(localStorage.getItem("runnersHi_user"))
-
+  const userActivities = activities.filter(a => a.userId === user)
+  console.log(userActivities)
   let moodsPre = []
+  //let moodsPre = [0];
+  
 
   const moodsPreTotal = () => {
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     activities.forEach(activity => {
       if (activity.userId === user) {
+        //moodsPre.splice(0, 1)
         moodsPre.push(parseInt(activity.moodPre))
       }
       console.log(moodsPre)
@@ -22,10 +26,14 @@ export default function ActivityMoodsMath() {
   }
 
   let moodsPost = []
+  //let moodsPost = [0];
+  
+    console.log(moodsPost)
   const moodsPostTotal = () => {
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     activities.forEach(activity => {
       if (activity.userId === user) {
+        //moodsPre.splice(0, 1)
         moodsPost.push(parseInt(activity.moodPost))
       }
       console.log(moodsPost)
@@ -34,7 +42,11 @@ export default function ActivityMoodsMath() {
     return moodsPost.reduce(reducer)
   }
 
-  const average = (moodsPostTotal() - moodsPreTotal()) / moodsPre.length
+  const dividedBy = userActivities.length
+  console.log(dividedBy)
+  const divisible = (moodsPostTotal() - moodsPreTotal())
+  console.log(divisible)
+  const average = divisible/dividedBy 
 
 
 
