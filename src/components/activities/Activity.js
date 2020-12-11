271fcf320435c9b7ff328807330874e1de32c1ba
Jasmin Kaset <kasetski@gmail.com>
2020-12-11 07:41:35 -0600

    math in Activity.js gives percentage per run

--- src/components/activities/Activity.js
+++ src/components/activities/Activity.js
@@ -11,12 +11,16 @@ export const Activity = ({ activity, activityType, props }) => {
     deleteActivity(activityId)
   }
   let activityHeading = (activityType.name).toUpperCase()
+
+  
+
   return (
     <section className="activity">
       <div className="activity__date">{activity.date}</div>
       <div className="activity__name">{activityHeading}</div>
       <div className="activity__moodPre">Pre-activity Mood: {activity.moodPre} out of 10</div>
       <div className="activity__moodPost">Post-activity Mood: {activity.moodPost} out of 10</div>
+      <div className="activity__moodChange">Your mood quotient changed {((activity.moodPost-activity.moodPre)/10)*100}%</div>
       <div className="activity__note">Note: "{activity.note}"</div>
 
 
