3f850f685adf07de6abdcaa7c3231d93d0344831
Jasmin Kaset <kasetski@gmail.com>
2020-12-12 08:22:54 -0600

    pre mood chart works, but data is a little funny

--- src/components/activities/ActivityChart.js
+++ src/components/activities/ActivityChart.js
@@ -1,41 +1,98 @@
-import React from 'react'
-import {Line} from 'react-chartjs-2'
+import React, { useContext } from 'react'
+import { ActivityContext } from "./ActivityProvider"
+import { Line } from 'react-chartjs-2'
 
-const state = {
-  labels: ['January', 'February', 'March',
-           'April', 'May'],
-  datasets: [
-    {
-      label: 'Rainfall',
-      fill: false,
-      lineTension: 0.5,
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [65, 59, 80, 81, 56]
-    }
-  ]
-}
+
 
 export const ActivityChart = () => {
-  
-    return (
-      <div>
-        <Line
-          data={state}
-          options={{
-            title:{
-              display:true,
-              text:'Average Rainfall per month',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
-      </div>
-    
-    )
+
+  //define user
+  const user = parseInt(localStorage.getItem("runnersHi_user"))
+  // const userActivities = activities.filter(a => a.userId === user)
+
+  //pull in user's activities
+  const { activities } = useContext(ActivityContext)
+
+  ////filter for activities specific to that user
+  const userActivities = (activities.filter(a => a.userId === user))
+
+  //console.log(userActivities)
+
+  let moodsPre = []
+
+  //push all moodPre numbers into empty array
+  activities.forEach(activity => {
+    if (activity.userId === user) {
+      moodsPre.push(parseInt(activity.moodPre))
+    }
+    //console.log(moodsPre)
+  })
+
+  // const instances = userActivities.length
+  // console.log(instances)
+let instancesArray = []
+//const instances = () => {
+  for (let i = 0; i < userActivities.length; i++) {
+    //console.log((i).toString()).split("")
+    instancesArray.push(((i).toString()).split(""))
+    //return i
+  }
+//}
+console.log(instancesArray)
+
+
+
+
+  const state = {
+    labels: (instancesArray),
+    datasets: [
+      {
+        label: 'Before',
+        fill: false,
+        lineTension: 0.5,
+        backgroundColor: 'rgba(75,192,192,1)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: (moodsPre)
+      }
+    ]
+  }
+
+  const state1 = {
+    labels: (instancesArray),
+    datasets: [
+      {
+        label: 'Before',
+        fill: false,
+        lineTension: 0.5,
+        backgroundColor: 'rgba(75,192,192,1)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: (6, 7, 8, 9, 10)
+      }
+    ]
+  }
+
+
+
+  return (
+    <>
+    <div>
+      <Line
+        data={state}
+        options={{
+          title: {
+            display: true,
+            text: 'Mood Relationship',
+            fontSize: 20
+          },
+          legend: {
+            display: true,
+            position: 'right'
+          }
+        }}
+      />
+    </div>
+</>
+  )
 }
