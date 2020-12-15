1b6c99e1fbf4a9e98553daa0a26a5d8d7812cf64
Jasmin Kaset <kasetski@gmail.com>
2020-12-15 10:28:47 -0600

    conditional logic for graph display

--- src/components/activities/ActivityList.js
+++ src/components/activities/ActivityList.js
@@ -3,11 +3,11 @@ import { ActivityContext } from "./ActivityProvider"
 import { Activity } from "./Activity"
 import { ActivityTypeContext } from "../activityTypes/ActivityTypeProvider"
 import ActivityMoodMath from "./ActivityMoodMath"
-import {Logout} from "../auth/Logout"
+import { Logout } from "../auth/Logout"
 import "./Activity.css"
 import { Link } from "react-router-dom"
 import { ActivityChart } from "./ActivityChart"
-import {Card, Accordion} from "react-bootstrap" 
+import { Card, Accordion } from "react-bootstrap"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
@@ -28,42 +28,72 @@ export const ActivityList = () => {
   const userActivities = activities.filter(a => a.userId === user)
   //console.log(userActivities)
 
-  if (userActivities.length > 0) {
+  if (userActivities.length >= 3) {
     return (
       <>
-        
-          <Link to="/activities/create" className="btn btn-warning"><FontAwesomeIcon icon={ faPlus }/> Record a New Run</Link>
-        
-       
+
+        <Link to="/activities/create" className="btn btn-warning"><FontAwesomeIcon icon={faPlus} /> Record a New Run</Link>
+
+
         <div>
           <h3>Running changes your mood by <ActivityMoodMath />% </h3>
           <ActivityChart />
           <h4>Your Stats</h4>
 
-        <div className="pre-scrollable">
-          {
-            activities.map(activity => {
-              if (activity.userId === user) {
-                const activityType = activityTypes.find(type => type.id === activity.activityTypeId)
+          <div className="pre-scrollable">
+            {
+              activities.map(activity => {
+                if (activity.userId === user) {
+                  const activityType = activityTypes.find(type => type.id === activity.activityTypeId)
 
-                return <Activity key={activity.id}
-                  activity={activity}
-                  activityType={activityType}
-                />
-              }
-            })}
+                  return <Activity key={activity.id}
+                    activity={activity}
+                    activityType={activityType}
+                  />
+                }
+              })}
           </div>
+        </div>
+
+      </>
+    )
+  } else if (userActivities.length < 3 && userActivities.length > 0) {
+    return (
+      <>
+
+        <Link to="/activities/create" className="btn btn-warning"><FontAwesomeIcon icon={faPlus} /> Record a New Run</Link>
+
+
+        <div>
+          <h3>Running changes your mood by <ActivityMoodMath />% </h3>
+
+          <h4>Your Stats</h4>
+
+          <div className="pre-scrollable">
+            {
+              activities.map(activity => {
+                if (activity.userId === user) {
+                  const activityType = activityTypes.find(type => type.id === activity.activityTypeId)
+
+                  return <Activity key={activity.id}
+                    activity={activity}
+                    activityType={activityType}
+                  />
+                }
+              })}
           </div>
-        
+        </div>
+
       </>
     )
   } else {
     return (
       <>
-       <Logout />
-      <div>
-      <Link to="/activities/create" className="btn btn-secondary"><FontAwesomeIcon icon={ faPlus }/> Record a New Run</Link>
-      </div>
+        <h4>Looks like you don't have any runs recorded yet! </h4>
+        <div>
+          <Link to="/activities/create" className="btn btn-secondary"><FontAwesomeIcon icon={faPlus} /> Record a New Run</Link>
+        </div>
+
       </>
     )
   }
